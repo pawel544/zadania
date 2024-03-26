c08cc91b3d015f8ceb882f5044c24835dd959b8a
pawel544 <140202153+pawel544@users.noreply.github.com>
2024-03-27 00:53:09 +0100

    Add files via upload

--- zadanie_9/main.py
+++ zadanie_9/main.py
@@ -2,7 +2,7 @@ import lxml
 import requests
 from bs4 import BeautifulSoup
 import json
-from mongoengine import *
+from mongoengine import connect
 
 connect( "autorzy", host="mongoDB+SRV://pawel:<hasło>@pawel.cjlubnv.mongodb.net/?retryWrites=true&w=majority&appName=autorzy")
 url=" http://quotes.toscrape.com"
@@ -14,44 +14,82 @@ def normalizer(text):
     normalized_text = normalized_text.replace("é", "e")
     return normalized_text
 
+class Name():
+    pass
+class Born_name():
+    pass
+class description():
+    pass
 
-class Author():
-    def __init__(self):
-        response= requests.get(url)
-        soup=BeautifulSoup(response.text, "lxml")
-        author=soup.find_all("small", class_='author')
-        self.authors_list = []
+def author(url):
+    response = requests.get(url)
+    authors_list = []
+    soup = BeautifulSoup(response.text, "lxml")
+    author = soup.find_all("small", class_='author')
+    for aut in author:
 
+        name=aut.text
+        normalized_text=normalizer(name)
+        url2 = f'{url}/author/{normalized_text}/'
+        response1 = requests.get(url2)
+        soup1 = BeautifulSoup(response1.text, "lxml")
+        born = soup1.find_all('span', class_='author-born-date')
+        text = soup.find_all('span', class_='text')
+        for tex in text:
+         description=tex.text
+        for bo in born:
+
+
+            born_date=bo.text
+            authors={"fullname:": name,
+            "born_date:": born_date,
+            "description": description}
+            authors_list.append(authors)
+    return authors_list
+
+
+def page(author1):
+    authors_list = []
+    page = 1
+    while True:
+        url3 = f"{url}/page/{page}"
+        response1 = requests.get(url3)
+
+
+        soup1 = BeautifulSoup(response1.text, "lxml")
+        author = soup1.find_all("small", class_='author')
+        if not author:
+            break
 
         for aut in author:
-            self.name=aut.text
-            normalized_text=normalizer(self.name)
-            self.url2 = f'{url}/author/{normalized_text}/'
 
-            response = requests.get(self.url2)
-            soup = BeautifulSoup(response.text, 'lxml')
+            name = aut.text
+            normalized_text = normalizer(name)
+            url2 = f'{url}/author/{normalized_text}/'
+            response = requests.get(url2)
+            soup = BeautifulSoup(response.text, "lxml")
             born = soup.find_all('span', class_='author-born-date')
+            text = soup1.find_all('span', class_='text')
 
-            response = requests.get(url)
-            soup = BeautifulSoup(response.text, 'lxml')
-            text = soup.find_all('span', class_='text')
+        for tex in text:
+         description=tex.text
 
+         for bo in born:
 
-            for tex in text:
-                self.description=tex.text
-                for bo in born:
+                born_date=bo.text
+                authors={"fullname:": name,
+                "born_date:": born_date,
+                "description": description}
+                authors_list.append(authors)
 
-                    self.born_date=bo.text
-                    authors={"fullname:": self.name,
-                    "born_date:": self.born_date,
-                    "description": self.description}
-                    self.authors_list.append(authors)
-
-
-
-author= Author()
+        page +=1
+    return authors_list
 
 
+
+author1 = author(url)
+authors_data=page(author1)
+print("Proces zakończony Sukcesem")
 with open("authors.json", "w") as f:
-        json.dump(author.authors_list, f, indent=4)
+        json.dump(authors_data, f, indent=4)
         f.write("\n")
