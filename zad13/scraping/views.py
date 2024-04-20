25df1fc26ca6e1170047183a80dd6cdb846fd92d
pawel544 <140202153+pawel544@users.noreply.github.com>
2024-04-20 21:21:58 +0200

    Add files via upload

--- zad13/scraping/views.py
+++ zad13/scraping/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 from bs4 import BeautifulSoup
 import requests
 from .models import Quote, Author
@@ -8,39 +8,37 @@ def normalizer(text):
     return normalized_text
 
 
+def scrap_and_fill_database():
+    url = 'http://quotes.toscrape.com'
 
+    response = requests.get(url)
 
-def scrap_and_fill_database(request):
-    if request.method == "POST":
-
-
-        url = 'http://quotes.toscrape.com'
-
-
-
-
-
-
-        authors_list = []
+    if response.status_code == 200:
         page_number = 1
+
         while True:
             response = requests.get(f"{url}/page/{page_number}")
-            soup = BeautifulSoup(response.text, "lxml")
+            soup = BeautifulSoup(response.text, 'lxml')
+
             authors = soup.find_all("small", class_='author')
+
             if not authors:
                 break
+
             for author in authors:
                 name = author.text
-                normalized_name = normalizer(name)
+                normalized_name = name.lower().replace(" ", "-")
                 author_url = f'{url}/author/{normalized_name}/'
+
                 response_author = requests.get(author_url)
                 soup_author = BeautifulSoup(response_author.text, 'lxml')
 
-                description_1 = soup_author.find('span', class_='author-description')
-                if description_1:
-                    description= description_1.text.strip()
+                description = soup_author.find('span', class_='author-description').text.strip()
+                db_author, created = Author.objects.get_or_create(name=name, defaults={'description': description})
+                quote_text = soup.find('span', class_='text').text
+                Quote.objects.create(text=quote_text, author=db_author)
             page_number += 1
-            return redirect(to='strona:main')
+
     else:
 
         print('Błąd: Nie udało się pobrać strony')
