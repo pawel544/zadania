eae4850132c2b5aee5d16023158479534746b996
pawel544 <140202153+pawel544@users.noreply.github.com>
2024-04-20 21:17:56 +0200

    Add files via upload

--- zadanie_10/scraping/views.py
+++ zadanie_10/scraping/views.py
@@ -8,37 +8,37 @@ def normalizer(text):
     return normalized_text
 
 
-
-
 def scrap_and_fill_database():
-
     url = 'http://quotes.toscrape.com'
-
 
     response = requests.get(url)
 
-
     if response.status_code == 200:
-
-        authors_list = []
         page_number = 1
+
         while True:
             response = requests.get(f"{url}/page/{page_number}")
-            soup = BeautifulSoup(response.text, "lxml")
+            soup = BeautifulSoup(response.text, 'lxml')
+
             authors = soup.find_all("small", class_='author')
+
             if not authors:
                 break
+
             for author in authors:
                 name = author.text
-                normalized_name = normalizer(name)
+                normalized_name = name.lower().replace(" ", "-")
                 author_url = f'{url}/author/{normalized_name}/'
+
                 response_author = requests.get(author_url)
                 soup_author = BeautifulSoup(response_author.text, 'lxml')
+
                 description = soup_author.find('span', class_='author-description').text.strip()
-                soup_author.save()
-                description.save()
+                db_author, created = Author.objects.get_or_create(name=name, defaults={'description': description})
+                quote_text = soup.find('span', class_='text').text
+                Quote.objects.create(text=quote_text, author=db_author)
             page_number += 1
-            return redirect(to='strona:main')
+
     else:
 
         print('Błąd: Nie udało się pobrać strony')
